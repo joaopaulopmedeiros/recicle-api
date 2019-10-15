70a5628fd059a45d4b6efc1d131e47f052cae799
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-10-15 17:47:32 -0300

    alteração no nome da coluna referente ao email

--- application/controllers/Cidadao.php
+++ application/controllers/Cidadao.php
@@ -16,7 +16,7 @@ class Cidadao extends CI_Controller {
  
     function inserir() {
         $this->form_validation->set_rules("nome", "Nome", "required");
-        $this->form_validation->set_rules("email", "Email", "required");
+        $this->form_validation->set_rules("login", "Login", "required");
         $this->form_validation->set_rules("cpf", "CPF", "required");
         $this->form_validation->set_rules("cep", "CEP", "required");
         $this->form_validation->set_rules("senha", "Senha", "required");
@@ -25,7 +25,7 @@ class Cidadao extends CI_Controller {
         if($this->form_validation->run()) {
             $data = array(
                 'nome'   => trim($this->input->post('nome')),
-                'email'  => trim($this->input->post('email')),
+                'login'  => trim($this->input->post('login')),
                 'cpf'    => trim($this->input->post('cpf')),
                 'cep'    => trim($this->input->post('cep')),
                 'senha'  => trim($this->input->post('senha'))
@@ -38,7 +38,7 @@ class Cidadao extends CI_Controller {
             $array = array(
                 'error'       => true,
                 'nome_error'  => form_error('nome'),
-                'email_error' => form_error('email'),
+                'login_error' => form_error('login'),
                 'cpf_error'   => form_error('cpf'),
                 'cep_error'   => form_error('cep'),
                 'senha_error' => form_error('senha')
