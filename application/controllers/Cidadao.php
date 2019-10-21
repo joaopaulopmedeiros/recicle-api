60915321129a6e963fd376d26d91c2a48219c097
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-10-21 09:19:54 -0300

    adicionand condições dos campos dos formulários

--- application/controllers/Cidadao.php
+++ application/controllers/Cidadao.php
@@ -20,34 +20,66 @@ class Cidadao extends CI_Controller
     {
         $this->form_validation->set_rules("nome", "Nome", "required");
         $this->form_validation->set_rules("login", "Login", "required");
-        $this->form_validation->set_rules("docCadastrado", "CPF", "required");
-        $this->form_validation->set_rules("cep", "CEP", "required");
-        $this->form_validation->set_rules("senha", "Senha", "required");
+        $this->form_validation->set_rules("docCadastrado", "CPF", "required|exact_length[11]");
+        $this->form_validation->set_rules("cep", "CEP", "required|exact_length[8]");
+        $this->form_validation->set_rules("senha", "Senha", "required|min_length[6]|max_length[20]");
+        $this->form_validation->set_rules("confirmarSenha", "Confirmar senha", "required|min_length[6]|max_length[20]");
         $array = array();
 
         if($this->form_validation->run()) 
         {
-            $data = array(
-                'nome'   => trim($this->input->post('nome')),
-                'login'  => trim($this->input->post('login')),
-                'docCadastrado'    => trim($this->input->post('docCadastrado')),
-                'cep'    => trim($this->input->post('cep')),
-                'senha'  => trim($this->input->post('senha'))
-            );
-            $this->cidadao_model->insert_api($data);
-            $array = array(
-                'success'  => true
-            );
+            $senha = $this->input->post('senha');
+            $confirmarSenha = $this->input->post('confirmarSenha');
+            $doc = $this->input->post('docCadastrado');
+            $login = $this->input->post('login');
+
+            if ($senha == $confirmarSenha)
+            {
+                if ($this->cidadao_model->verificarDocCadastrado($doc) == null)
+                {
+                    if ($this->cidadao_model->verificarEmail($login) == null)
+                    {
+                        $data = array(
+                            'nome' => trim($this->input->post('nome')),
+                            'login' => trim($this->input->post('login')),
+                            'docCadastrado' => trim($this->input->post('docCadastrado')),
+                            'cep' => trim($this->input->post('cep')),
+                            'senha' => trim($this->input->post('senha'))
+                        );
+                        $this->cidadao_model->insert_api($data);
+                        $array = array(
+                            'success' => true
+                        );
+                    }
+                    else
+                    {
+                        $array = array(
+                            'error' => true,
+                            'msg_erro' => 'O email inserido no formulário já está cadastrado no sistema.'
+                        );
+                    }
+                }
+                else
+                {
+                    $array = array(
+                        'error' => true,
+                        'msg_erro' => 'O documento de identificação inserido no formulário já está cadastrado no sistema.'
+                    );
+                }
+            }
+            else
+            {
+                $array = array(
+                    'error' => true,
+                    'msg_erro' => 'Os campos da senha não estão iguais.'
+                );
+            }
         }
         else 
         {
             $array = array(
-                'error'       => true,
-                'nome_error'  => form_error('nome'),
-                'login_error' => form_error('login'),
-                'docCadastrado_error'   => form_error('docCadastrado'),
-                'cep_error'   => form_error('cep'),
-                'senha_error' => form_error('senha')
+                'error' => true,
+                'msg_erro' => 'Preencha todos os dados corretamente.'
             );
         }
         echo json_encode($array, true);
--- application/controllers/CriadorDesafio.php
+++ application/controllers/CriadorDesafio.php
@@ -17,39 +17,76 @@ class CriadorDesafio extends CI_Controller {
     function inserir() {
         $this->form_validation->set_rules("nome", "Nome", "required");
         $this->form_validation->set_rules("login", "Login", "required");
-        $this->form_validation->set_rules("docCadastrado", "Documento Cadastrado", "required");
-        $this->form_validation->set_rules("cep", "CEP", "required");
-        $this->form_validation->set_rules("senha", "Senha", "required");
+        $this->form_validation->set_rules("docCadastrado", "CPF", "required|exact_length[14]");
+        $this->form_validation->set_rules("cep", "CEP", "required|exact_length[8]");
+        $this->form_validation->set_rules("senha", "Senha", "required|min_length[6]|max_length[20]");
+        $this->form_validation->set_rules("confirmarSenha", "Confirmar senha", "required|min_length[6]|max_length[20]");
         $array = array();
 
-        if($this->form_validation->run()) {
-            $data = array(
-                'nome'   => trim($this->input->post('nome')),
-                'login'  => trim($this->input->post('login')),
-                'docCadastrado'    => trim($this->input->post('docCadastrado')),
-                'cep'    => trim($this->input->post('cep')),
-                'senha'  => trim($this->input->post('senha'))
-            );
-            $this->CriadorDesafio_model->insert_api($data);
+        if($this->form_validation->run()) 
+        {
+            $senha = $this->input->post('senha');
+            $confirmarSenha = $this->input->post('confirmarSenha');
+            $doc = $this->input->post('docCadastrado');
+            $login = $this->input->post('login');
+
+            if ($senha == $confirmarSenha)
+            {
+                if ($this->CriadorDesafio_model->verificarDocCadastrado($doc) == null)
+                {
+                    if ($this->CriadorDesafio_model->verificarEmail($login) == null)
+                    {
+                        $data = array(
+                            'nome' => trim($this->input->post('nome')),
+                            'login' => trim($this->input->post('login')),
+                            'docCadastrado' => trim($this->input->post('docCadastrado')),
+                            'cep' => trim($this->input->post('cep')),
+                            'senha' => trim($this->input->post('senha'))
+                        );
+                        $this->CriadorDesafio_model->insert_api($data);
+                        $array = array(
+                            'success' => true
+                        );
+                    }
+                    else
+                    {
+                        $array = array(
+                            'error' => true,
+                            'msg_erro' => 'O email inserido no formulário já está cadastrado no sistema.'
+                        );
+                    }
+                }
+                else
+                {
+                    $array = array(
+                        'error' => true,
+                        'msg_erro' => 'O CPF ou CNPJ inserido no formulário já está cadastrado no sistema.'
+                    );
+                }
+            }
+            else
+            {
+                $array = array(
+                    'error' => true,
+                    'msg_erro' => 'Os campos da senha não estão iguais.'
+                );
+            }
+        }
+        else 
+        {
             $array = array(
-                'success'  => true
-            );
-        } else {
-            $array = array(
-                'error'       => true,
-                'nome_error'  => form_error('nome'),
-                'login_error' => form_error('login'),
-                'docCadastrado_error'   => form_error('docCadastrado'),
-                'cep_error'   => form_error('cep'),
-                'senha_error' => form_error('senha')
+                'error' => true,
+                'msg_erro' => 'Preencha todos os dados corretamente.'
             );
         }
         echo json_encode($array, true);
     }
 
-    function fetch_single() {
-        if($this->input->post('docCadastrado')) {
-            $data = $this->CriadorDesafio_model->fetch_single_user($this->input->post('id'));
+    function fetch_single()
+    {
+        if($this->input->post('docCadastrado'))
+        {
+            $data = $this->criadorDesafio_model->fetch_single_user($this->input->post('id'));
             foreach($data as $row) {
                 $output['nome'] = $row["nome"];
                 $output['email'] = $row["email"];
@@ -61,15 +98,17 @@ class CriadorDesafio extends CI_Controller {
         }
     }
 
-    function update() {
+    function update()
+    {
         $this->form_validation->set_rules("nome", "Nome", "required");
         $this->form_validation->set_rules("email", "Email", "required");
-        $this->form_validation->set_rules("docCadastrado", "docCadastrado", "required");
+        $this->form_validation->set_rules("docCadastrado", "CPF", "required");
         $this->form_validation->set_rules("cep", "CEP", "required");
         $this->form_validation->set_rules("senha", "Senha", "required");
         $array = array();
 
-        if($this->form_validation->run()) {
+        if($this->form_validation->run())
+        {
             $data = array(
                 'nome' => trim($this->input->post('nome')),
                 'email'  => trim($this->input->post('email')),
@@ -77,13 +116,15 @@ class CriadorDesafio extends CI_Controller {
                 'cep'  => trim($this->input->post('cep')),
                 'senha'  => trim($this->input->post('senha'))
             );
-            $this->CriadorDesafio_model->update_api($this->input->post('docCadastrado'), $data);
+            $this->criadorDesafio_model->update_api($this->input->post('docCadastrado'), $data);
             $array = array(
                 'success'  => true
             );
-        } else {
+        }
+        else
+        {
             $array = array(
-                'error'            => true,
+                'error' => true,
                 'nome_error' => form_error('nome'),
                 'email_error'  => form_error('email'),
                 'docCadastrado_error' => form_error('docCadastrado'),
