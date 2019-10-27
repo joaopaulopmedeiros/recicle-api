3ed70894852843ec3a54f274a4926b3be11b2b1f
joao paulo <joaopaulopmedeiros@gmail.com>
2019-10-27 10:40:09 -0300

    load models automatically

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('Cidadao_model','CriadorDesafio_model');
