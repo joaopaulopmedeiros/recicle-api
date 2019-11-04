e4a1be4446abdcee781095ff85e833a0fc7941a8
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-04 00:35:33 -0300

    adição do método exibir meus desafios

--- application/controllers/Desafios.php
+++ application/controllers/Desafios.php
@@ -10,9 +10,21 @@ class Desafios extends CI_Controller
         $this->load->library('form_validation');
     }
 
-    function index() 
+    function exibirTodosOsDesafios() 
     {
         $data = $this->desafio_model->fetch_all();
         echo json_encode($data->result_array());
+    }
+
+    function exibirMeusDesafios()
+    {
+        $user_id = $this->input->post('user_id');
+        $data = $this->desafio_model->exibir_meus_desafios($user_id);
+        echo json_encode($data->result_array());
+    }
+
+    function inserir()
+    {
+        
     }
 }
--- application/models/Desafio_model.php
+++ application/models/Desafio_model.php
@@ -25,11 +25,16 @@ class Desafio_model extends CI_Model
     }
   }
 
-  function fetch_single($id)
+  function exibir_meus_desafios($idCriadorDesafio)
   {
-    $this->db->where("id", $id);
-    $query = $this->db->get('desafio');
-    return $query->result_array();
+    $this->db->select('desafio.*, criadorDesafio.nome, criadorDesafio.cep, rsu.tipo, bonificacao.nome');    
+    $this->db->from('desafio');
+    $this->db->where("desafio.idCriadorDesafio", $idCriadorDesafio);
+    $this->db->join('criadorDesafio', 'criadorDesafio.docCadastrado = desafio.idCriadorDesafio');
+    $this->db->join('rsu', 'desafio.idTipoRSU = rsu.id');
+    $this->db->join('bonificacao', 'desafio.idTipoBonificacao = bonificacao.id');
+    $this->db->order_by('desafio.id', 'DESC');
+    return $this->db->get();
   }
 
   function update_api($id, $data)
