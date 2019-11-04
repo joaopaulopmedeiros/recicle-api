0dbc19647058fb5c97383ea02a11a43fe872d449
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-11-04 11:10:54 -0200

    adding insert method

--- application/controllers/RSU.php
+++ application/controllers/RSU.php
@@ -15,4 +15,41 @@ class RSU extends CI_Controller
         $data = $this->rsu_model->fetch_all();
         echo json_encode($data->result_array());
     }
+
+    function insert()
+    {
+        $this->form_validation->set_rules("tipo", " Tipo", "required");
+        $array = array();
+
+        if($this->form_validation->run()) 
+        {
+            $tipo = $this->input->post('tipo');
+
+            if ($this->rsu_model->verificarTipoLixo($tipo) == null)
+            {
+                $data = array(
+                    'tipo' => trim($this->input->post('tipo'))
+                );
+                $this->rsu_model->insert_api($data);
+                    $array = array(
+                        'success' => true
+                    );
+                }
+            else
+            {
+                $array = array(
+                    'error' => true,
+                    'msg_erro' => 'O tipo de lixo informado já está cadastrado no sistema.'
+                );
+            }
+        }
+        else 
+        {
+            $array = array(
+                'error' => true,
+                'msg_erro' => 'Este campo deve ser preenchido.'
+            );
+        }
+        echo json_encode($array, true);
+    }
 }
