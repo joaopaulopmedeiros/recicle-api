808c73a9aea512f85f605b06e4066d4659ad69e8
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-04 23:33:29 -0300

    adding method to insert in database

--- application/controllers/Bonificacao.php
+++ application/controllers/Bonificacao.php
@@ -15,4 +15,41 @@ class Bonificacao extends CI_Controller
         $data = $this->bonificacao_model->fetch_all();
         echo json_encode($data->result_array());
     }
+
+    function insert()
+    {
+        $this->form_validation->set_rules("nome", " Nome", "required");
+        $array = array();
+
+        if($this->form_validation->run()) 
+        {
+            $nome = $this->input->post('nome');
+
+            if ($this->bonificacao_model->verificarBonificacao($nome) == null)
+            {
+                $data = array(
+                    'nome' => trim($this->input->post('nome'))
+                );
+                $this->bonificacao_model->insert_api($data);
+                $array = array(
+                    'success' => true
+                );
+            }
+            else
+            {
+                $array = array(
+                    'error' => true,
+                    'msg_erro' => 'A bonificação inserida já está cadastrada no sistema.'
+                );
+            }
+        }
+        else 
+        {
+            $array = array(
+                'error' => true,
+                'msg_erro' => 'Este campo deve ser preenchido.'
+            );
+        }
+        echo json_encode($array, true);
+    }
 }
