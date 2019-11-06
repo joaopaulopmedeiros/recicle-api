f7feb119727b6a6ab46e8d67eb2ff3aeb29dc64f
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-05 22:12:57 -0300

    adding 'as' to query sql

--- application/models/Desafio_model.php
+++ application/models/Desafio_model.php
@@ -3,7 +3,7 @@ class Desafio_model extends CI_Model
 {
   function fetch_all()
   {
-    $this->db->select('desafio.*, criadorDesafio.nome, criadorDesafio.cep, rsu.tipo, bonificacao.nome');    
+    $this->db->select('desafio.*, criadorDesafio.nome as criador_desafio, criadorDesafio.cep as cep_criador_desafio, rsu.tipo as tipo_rsu, bonificacao.nome as tipo_bonificacao');    
     $this->db->from('desafio');
     $this->db->join('criadorDesafio', 'criadorDesafio.docCadastrado = desafio.idCriadorDesafio');
     $this->db->join('rsu', 'desafio.idTipoRSU = rsu.id');
@@ -27,7 +27,7 @@ class Desafio_model extends CI_Model
 
   function exibir_meus_desafios($idCriadorDesafio)
   {
-    $this->db->select('desafio.*, criadorDesafio.nome, criadorDesafio.cep, rsu.tipo, bonificacao.nome');    
+    $this->db->select('desafio.*, criadorDesafio.nome as criador_desafio, criadorDesafio.cep as cep_criador_desafio, rsu.tipo as tipo_rsu, bonificacao.nome as tipo_bonificacao');    
     $this->db->from('desafio');
     $this->db->where("desafio.idCriadorDesafio", $idCriadorDesafio);
     $this->db->join('criadorDesafio', 'criadorDesafio.docCadastrado = desafio.idCriadorDesafio');
