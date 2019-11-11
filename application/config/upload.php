d1cbae758f2aa17f24eefe5dc906cde99852d108
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-11 06:38:49 -0300

    iniciando implementação de upload de imagens

---
+++ application/config/upload.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['upload_path'] = './assets/images';
+$config['allowed_types'] = 'jpg|png';
+$config['max_size'] = '5000';
+$config['max_width']  = '1920';
+$config['max_height']  = '1080';
--- application/controllers/Desafios.php
+++ application/controllers/Desafios.php
@@ -7,7 +7,6 @@ class Desafios extends CI_Controller
     {
         parent::__construct();
         $this->load->model('desafio_model');
-        $this->load->library('form_validation');
     }
 
     function exibirTodosOsDesafios() 
@@ -29,38 +28,43 @@ class Desafios extends CI_Controller
         $this->form_validation->set_rules("descricao", "descricao", "required");
         $this->form_validation->set_rules("idCriadorDesafio", "idCriadorDesafio", "required");
         $this->form_validation->set_rules("idTipoRSU", "idTipoRSU", "required");
-
+        
         $array = array();
 
         if ($this->form_validation->run())
         {
-            $data = array(
-                'titulo' => trim($this->input->post('titulo')),
-                'descricao' => trim($this->input->post('descricao')),
-                'idCriadorDesafio' => trim($this->input->post('idCriadorDesafio')),
-                'idTipoBonificacao' => trim($this->input->post('idTipoBonificacao')),
-                'idTipoRSU' => trim($this->input->post('idTipoRSU')),
-                'qtdRSU' => trim($this->input->post('qtdRSU')),
-                'descricaoBonificacao' => trim($this->input->post('descricaoBonificacao')),
-                'dataLimite' => trim($this->input->post('dataLimite')),
-                'img' => trim($this->input->post('img'))
-            );
-            $this->desafio_model->insert_api($data);
-            $array = array('success' => true);
+            if ($this->upload->do_upload('img'))
+            {
+                $data_img = $this->upload->data();
+                $image = $data_img['file_name']; 
+                
+                $data = array(
+                    'titulo' => trim($this->input->post('titulo')),
+                    'descricao' => trim($this->input->post('descricao')),
+                    'idCriadorDesafio' => trim($this->input->post('idCriadorDesafio')),
+                    'idTipoBonificacao' => trim($this->input->post('idTipoBonificacao')),
+                    'idTipoRSU' => trim($this->input->post('idTipoRSU')),
+                    'qtdRSU' => trim($this->input->post('qtdRSU')),
+                    'descricaoBonificacao' => trim($this->input->post('descricaoBonificacao')),
+                    'dataLimite' => trim($this->input->post('dataLimite')),
+                    'img' => $data_img
+                );
+                $this->desafio_model->insert_api($data);
+                $array = array('success' => true);
+            }
+            else
+            {
+                $array = array(
+                    'error' => true,
+                    'msg_erro' => $this->upload->display_errors()
+                );
+            }
         }
         else
         {
             $array = array(
                 'error' => true,
-                'msg-erro' => 'Preencha todos os dados corretamente',
-                'titulo_error' => form_error('titulo'),
-                'descricao_error'  => form_error('descricao'),
-                'idCriadorDesafio_error' => form_error('idCriadorDesafio'),
-                'idTipoRSU_error'  => form_error('idTipoRSU'),
-                'qtdRSU_error' => form_error('qtdRSU'),
-                'descricaoBonificacao_error' => form_error('descricaoBonificacao'),
-                'dataLimite_error' => form_error('dataLimite'),
-                'img_error' => form_error('img')
+                'msg_erro' => 'Preencha todos os dados corretamente'
             );
         }
 
