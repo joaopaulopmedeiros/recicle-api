4de25bfec95fb2660af954f8caab3da310c58a91
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-11 22:49:27 -0300

    fix upload_path

--- application/config/upload.php
+++ application/config/upload.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['upload_path'] = './assets/images';
+$config['upload_path'] = './assets/img-desafio';
 $config['allowed_types'] = 'jpg|png';
 $config['max_size'] = '5000';
 $config['max_width']  = '1920';
